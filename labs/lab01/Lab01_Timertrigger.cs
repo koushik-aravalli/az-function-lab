8fe6c9afb6010a4a57c1db15b42fce7bd2b63727
Aravalli <Koushik.Aravalli@tatasteeleurope.com>
2019-08-20 22:46:33 +0200

    added timer trigger azure function

---
+++ labs/lab01/Lab01_Timertrigger.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace az203.labs.Function
+{
+    public static class Lab01_Timertrigger
+    {
+        [FunctionName("Lab01_Timertrigger")]
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
